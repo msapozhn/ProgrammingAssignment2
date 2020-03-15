8778a04ba59a033d8a113d003b44ce4dd96c9d89
msapozhn <62214935+msapozhn@users.noreply.github.com>
2020-03-15 14:23:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#######################
+## Maxim Sapozhnikov ##
+#######################
+## Week 3 Assignment ##
+#######################
 
-## Write a short comment describing this function
+## Creates object of type "Matrix with inversion cache"
+## Object functions available:
+##   get       returns stored value
+##   set       sets stored value, erases cache
+##   getsolve  returns stored cache
+##   setsolve  sets stored cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cache <- NULL
+    set <- function(m) {
+        x <<- m
+        cache <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setsolve <- function(m) {
+        cache <<- m
+    }
+    getsolve <- function {
+        cache
+    }
 }
 
-
-## Write a short comment describing this function
+## Calculates cache if necessary
+## Returns NULL and throws a warning if matrix is not square
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mout <- x$getsolve()
+    if (is.null(mout)) {
+        m <- x$get()
+        if (length(dim(m)) == 2 & dim(m)[1] == dim(m)[2]) {
+            mout <- solve(m, ...)
+        }
+        else {
+            warning("X: not a square matrix")
+        }
+        x$setsolve(mout)
+    }
+    mout
 }
